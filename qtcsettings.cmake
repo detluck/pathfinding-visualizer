487c76d8bb0c25fac08a0e8f9e30d3cdec283533
detluck <pashundel1987@gmail.com>
2026-04-08 13:25:29 +0200

    web build init

--- qtcsettings.cmake
+++ qtcsettings.cmake
@@ -1,2 +1,13 @@
 # This file is managed by Qt Creator, do not edit!
 
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/Projects/tools/emsdk/upstream/emscripten/em++.bat" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "C:/Qt/6.10.2/wasm_singlethread/bin/qmake.bat" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_TOOLCHAIN_FILE" "C:/Qt/6.10.2/wasm_singlethread/lib/cmake/Qt6/qt.toolchain.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Projects/Main/pathfinding-visualizer/build/WebAssembly_Qt_6_10_2_single_threaded-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_MAINTENANCE_TOOL" "C:/Qt/MaintenanceTool.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Projects/tools/emsdk/upstream/emscripten/emcc.bat" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/Qt/6.10.2/wasm_singlethread" CACHE "PATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
