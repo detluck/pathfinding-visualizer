45f571d3c5f3fd31c01e109c60f9e10350c74e6e
Vladyslav Marynenko <vlad.marinenko.98@gmail.com>
2026-04-15 22:55:57 +0200

    added dijkstra algorithm

---
+++ cpp/algorithms/dijkstra.h
@@ -0,0 +1,44 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+#include "ialgorithm.h"
+#include <queue>
+
+struct NodeRecord {
+    int index;
+    int cost;
+
+    bool operator>(const NodeRecord& other) const {
+        return cost > other.cost;
+    }
+};
+
+class Dijkstra: public IAlgorithm
+{
+public:
+    ~Dijkstra() override = default;
+    void run(const GridData& data) override;
+    void setState(const AlgoState algoState) override;
+    void init(const GridData& data) override;
+    StepResult step() override;
+    void blazzingRun() override;
+    std::vector<int> getPath() override;
+    AlgoState state() override;
+
+private:
+    std::vector<int> neighbors(int current) override;
+    void processNode(int current) override;
+
+private:
+    std::vector<int> m_cameFrom;
+    
+    std::priority_queue<NodeRecord, std::vector<NodeRecord>, std::greater<NodeRecord>> m_queue;
+    
+    std::vector<int> m_costs;
+
+    std::vector<bool> m_visited;
+    GridData m_data;
+    AlgoState m_state = AlgoState::Stopped;
+};
+
+
+#endif // DIJKSTRA_H
